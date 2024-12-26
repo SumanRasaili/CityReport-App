66b1b24d995fa2989e57ae0ea915159ed1622d5d
SumanRasaili <rasailisuman123@gmail.com>
2024-12-26 23:19:56 +0545

    debugbanner removed

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'City Report',
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
