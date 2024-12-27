5ab8df9b6ea45f5bf43beee085a37acbbf251960
SumanRasaili <rasailisuman123@gmail.com>
2024-12-27 14:18:12 +0545

    transaction bloc added

--- lib/presentation/blocs/transaction_report/transaction_report_bloc.dart
+++ lib/presentation/blocs/transaction_report/transaction_report_bloc.dart
@@ -1,14 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:citytech/core/error_handling.dart';
 import 'package:citytech/data/models/transaction_report_model.dart';
+import 'package:citytech/domain/repositories/transaction_repositories_interface.dart';
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 
 part 'transaction_report_event.dart';
 part 'transaction_report_state.dart';
 
-class TransactionReportBloc extends Bloc<TransactionReportEvent, TransactionReportState> {
-  TransactionReportBloc() : super(TransactionReportInitial()) {
-    on<TransactionReportEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class TransactionReportBloc
+    extends Bloc<TransactionReportEvent, TransactionReportState> {
+  final TransactionRepositoriesInterface transactionRepositoriesInterface;
+  TransactionReportBloc(this.transactionRepositoriesInterface)
+      : super(TransactionReportInitial()) {
+    on<TransactionReportEvent>(_fetchTransactionReport);
+  }
+  Future<void> _fetchTransactionReport(TransactionReportEvent event,
+      Emitter<TransactionReportState> emit) async {
+    try {
+      emit(TransactionReortLoading());
+      final transactionList =
+          await transactionRepositoriesInterface.getTransactions();
+      emit(TransactionReportLoaded(transactionList));
+    } on DioException catch (e) {
+      emit(TransactionReportError(await ErrorHandler().handleError(e)));
+    } catch (e) {
+      emit(TransactionReportError(e.toString()));
+    }
   }
 }
