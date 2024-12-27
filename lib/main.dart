148a084918d2c61b16826ecec52deacc66253ec7
SumanRasaili <rasailisuman123@gmail.com>
2024-12-27 14:20:58 +0545

    transaction bloc dependency added in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:citytech/config/themes.dart';
 import 'package:citytech/core/di_container/di_locator.dart';
+import 'package:citytech/presentation/blocs/transaction_report/transaction_report_bloc.dart';
 import 'package:citytech/presentation/screens/home_screen.dart';
 import 'package:citytech/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,15 +17,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'City Report',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomeScreen(),
-      },
-      theme: AppThemes.lightTheme,
+    return BlocProvider(
+      create: (context) => TransactionReportBloc(
+        locator(),
+      ),
+      child: MaterialApp(
+        title: 'City Report',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/home': (context) => const HomeScreen(),
+        },
+        theme: AppThemes.lightTheme,
+      ),
     );
   }
 }
