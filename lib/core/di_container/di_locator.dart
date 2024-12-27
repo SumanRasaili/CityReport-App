5fad4cbc4549b56d62b8d437fbf8b33f148473bc
SumanRasaili <rasailisuman123@gmail.com>
2024-12-27 13:57:17 +0545

    api serv to di

--- lib/core/di_container/di_locator.dart
+++ lib/core/di_container/di_locator.dart
@@ -1,4 +1,5 @@
 import 'package:citytech/core/dio/dio_manager.dart';
+import 'package:citytech/data/data_providers/api_service.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
@@ -6,5 +7,8 @@ final locator = GetIt.instance;
 Future<void> setUpLocator() async {
   locator.registerLazySingleton(
     () => DioManager(),
+  );
+  locator.registerLazySingleton(
+    () => ApiService(locator()),
   );
 }
