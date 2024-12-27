1affeccfefe689761e18d8373fa514d0b5e3f620
SumanRasaili <rasailisuman123@gmail.com>
2024-12-27 14:05:28 +0545

    repo in di locator

--- lib/core/di_container/di_locator.dart
+++ lib/core/di_container/di_locator.dart
@@ -1,5 +1,7 @@
 import 'package:citytech/core/dio/dio_manager.dart';
 import 'package:citytech/data/data_providers/api_service.dart';
+import 'package:citytech/data/repositories/transaction_repo_implementation.dart';
+import 'package:citytech/domain/repositories/transaction_repositories_interface.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
@@ -10,5 +12,9 @@ Future<void> setUpLocator() async {
   );
   locator.registerLazySingleton(
     () => ApiService(locator()),
+  );
+  //repo
+  locator.registerLazySingleton<TransactionRepositoriesInterface>(
+    () => TransactionRepoImplementation(locator()),
   );
 }
