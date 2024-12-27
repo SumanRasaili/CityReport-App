afacbc7117ffe683ecc6e83ac224ae1eb15eb4a8
SumanRasaili <rasailisuman123@gmail.com>
2024-12-27 22:28:06 +0545

    locator improved

--- lib/core/di_container/di_locator.dart
+++ lib/core/di_container/di_locator.dart
@@ -4,15 +4,18 @@ import 'package:citytech/data/repositories/outlet_repo_impl.dart';
 import 'package:citytech/data/repositories/transaction_repo_impl.dart';
 import 'package:citytech/domain/repositories/outlet_report_interfaces.dart';
 import 'package:citytech/domain/repositories/transaction_repositories_interface.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
 
 Future<void> setUpLocator() async {
+  locator.registerSingleton<Dio>(Dio());
+
   locator.registerLazySingleton<DioManager>(
     () => DioManager(),
   );
-  locator<DioManager>().setUpDio();
+
   //apiservice
   locator.registerLazySingleton(
     () => ApiService(locator<DioManager>().dio),
@@ -20,9 +23,9 @@ Future<void> setUpLocator() async {
 
   //repo
   locator.registerLazySingleton<TransactionRepositoriesInterface>(
-    () => TransactionRepoImplementation(locator<ApiService>()),
+    () => TransactionRepoImplementation(locator()),
   );
   locator.registerLazySingleton<OutletReportInterfaces>(
-    () => OutletRepoImpl(locator<ApiService>()),
+    () => OutletRepoImpl(locator()),
   );
 }
